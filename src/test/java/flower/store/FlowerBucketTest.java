35326a8f1046f801c5874e726330f3062e85d9af
rhusiev <h.radomyr@proton.me>
2023-10-11 15:55:45 +0300

    Prettify tests

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -1,15 +1,15 @@
 package flower.store;
 
+import java.util.Random;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-
-import java.util.Random;
 
 public class FlowerBucketTest {
     private static final Random RANDOM_GENERATOR = new Random();
     private static final int MAX_QUANTITY = 1000;
     private static final int MAX_PRICE = 100;
+    private static final int DEFAULT_PRICE = 10;
 
     private FlowerBucket flowerBucket;
 
@@ -22,8 +22,8 @@ public class FlowerBucketTest {
     public void testPrice() {
         int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
         int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
-        Flower flower = new Rose();
-        flower.setPrice(10);
+        Flower flower = new Flower(FlowerType.ROSE);
+        flower.setPrice(DEFAULT_PRICE);
         FlowerPack flowerPack = new FlowerPack(flower, quantity);
         flowerBucket.add(flowerPack);
         Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
