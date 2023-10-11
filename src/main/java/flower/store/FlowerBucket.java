e963bc79553bcd1ab32d9f5dc3f5f46243b3abbc
rhusiev <h.radomyr@proton.me>
2023-10-11 15:54:40 +0300

    Add add method to FlowerBucket

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class FlowerBucket {
     private List<Item> items;
 
-    public double price() {
+    public void add(Item item) {
+        items.add(item);
+    }
+
+    public void add(List<Item> items) {
+        this.items.addAll(items);
+    }
+
+    public double getPrice() {
         double price = 0;
         for (Item item : items) {
             price += item.getPrice();
